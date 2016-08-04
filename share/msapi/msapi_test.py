581af422eb07fe49eff0a2f7db1ae1cce6501c0a
chjeong530 <chjeong530@gmail.com>
2016-08-04 23:36:16 +0900

    add

--- share/msapi/msapi_test.py
+++ share/msapi/msapi_test.py
@@ -44,36 +44,39 @@ def processRequest( json, data, headers, params ):
     result = None
 
     while True:
+        try:
+            response = requests.request( 'post', _url, json = json, data = data, headers = headers, params = params )
+        except :
+            print ("error")
+        else :            
+            if response.status_code == 429: 
 
-        response = requests.request( 'post', _url, json = json, data = data, headers = headers, params = params )
+                print( "Message: %s" % ( response.json()['error']['message'] ) )
 
-        if response.status_code == 429: 
+                if retries <= _maxNumRetries: 
+                    time.sleep(1) 
+                    retries += 1
+                    continue
+                else: 
+                    print( 'Error: failed after retrying!' )
+                    break
 
-            print( "Message: %s" % ( response.json()['error']['message'] ) )
+            elif response.status_code == 200 or response.status_code == 201:
 
-            if retries <= _maxNumRetries: 
-                time.sleep(1) 
-                retries += 1
-                continue
-            else: 
-                print( 'Error: failed after retrying!' )
-                break
+                if 'content-length' in response.headers and int(response.headers['content-length']) == 0: 
+                    result = None 
+                elif 'content-type' in response.headers and isinstance(response.headers['content-type'], str): 
+                    if 'application/json' in response.headers['content-type'].lower(): 
+                        result = response.json() if response.content else None 
+                    elif 'image' in response.headers['content-type'].lower(): 
+                        result = response.content
+            else:
+                print( "Error code: %d" % ( response.status_code ) )
+                print( "Message: %s" % ( response.json()['error']['message'] ) )
 
-        elif response.status_code == 200 or response.status_code == 201:
-
-            if 'content-length' in response.headers and int(response.headers['content-length']) == 0: 
-                result = None 
-            elif 'content-type' in response.headers and isinstance(response.headers['content-type'], str): 
-                if 'application/json' in response.headers['content-type'].lower(): 
-                    result = response.json() if response.content else None 
-                elif 'image' in response.headers['content-type'].lower(): 
-                    result = response.content
-        else:
-            print( "Error code: %d" % ( response.status_code ) )
-            print( "Message: %s" % ( response.json()['error']['message'] ) )
-
-        break
+            break
         
+
     return result
 
 
@@ -175,7 +178,7 @@ import os
 pwd = '/home/maru/Project/modu/data'
 res = search(pwd)
 
-print (res)
+#print (res)
 
 # In[19]:
 
